2cce33d77e1d3980c04e8fad323fe4c5dd52429f
Minkyu Shim <minkyu.shim@net-targets.com>
2024-11-21 00:12:25 +0100

    Add a name column parameter

--- neo4jtools/utils.py
+++ neo4jtools/utils.py
@@ -9,10 +9,10 @@ import json
 import pandas as pd
 from collections import OrderedDict
 
-def parse_result_row_graph(jsonobj):
+def parse_result_row_graph(jsonobj, name_col='name'):
     '''
     Parse Neo4j HTTP API Graph Result row
-
+    
     input: element of result list
     output: tuple of nodes and edges
     '''
@@ -23,12 +23,12 @@ def parse_result_row_graph(jsonobj):
             label=n['labels'][0]
             props=n['properties']
             props['label']=label
-
+            
             keys=props.keys()
-            headcols=['identifier','name','label']
+            headcols=['identifier',name_col,'label']
             orderedkeys=headcols+list(set(keys)-set(headcols))
             orderedobj={k: props[k] for k in orderedkeys}
-
+            
             node_dict[_id]=orderedobj
 
         return node_dict
@@ -46,9 +46,9 @@ def parse_result_row_graph(jsonobj):
             edge_dict={
                 'type': _type,
                 'start_identifier':start_node['identifier'],
-                'start_name': start_node['name'],
+                'start_name': start_node[name_col],
                 'end_identifier':end_node['identifier'],
-                'end_name': end_node['name']
+                'end_name': end_node[name_col]
             }
 
             # inject the rest properties
@@ -59,12 +59,12 @@ def parse_result_row_graph(jsonobj):
             edges.append(edge_dict)
 
         return edges
-
+    
     gobj=jsonobj['graph']
-
-    node_dict=parse_nodes(gobj)
+    
+    node_dict=parse_nodes(gobj)    
     nodes=[node_dict[k] for k in node_dict]
-
+    
     edges=parse_edges(gobj, node_dict)
 
     return nodes, edges
@@ -73,28 +73,28 @@ def drop_duplicates(items):
     df=pd.DataFrame(items)
     df=df.drop_duplicates()
     items=df.to_dict(orient='records')
-
+    
     return items
 
-def parse_result_graph(data):
+def parse_result_graph(data, name_col='name'):
     '''
     Parse Neo4j HTTP API Graph Result
     '''
     nodes=[]
     edges=[]
     for item in data:
-        _nodes, _edges = parse_result_row_graph(item)
+        _nodes, _edges = parse_result_row_graph(item, name_col)
         nodes=nodes+_nodes
         edges=edges+_edges
-
+    
     nodes=drop_duplicates(nodes)
     edges=drop_duplicates(edges)
-
+    
     return nodes, edges
 
-def graph_renderer(neo4j_output):
+def graph_renderer(neo4j_output, name_col='name'):
     output=neo4j_output['results'][0]
-    nodes, edges = parse_result_graph(output['data'])
+    nodes, edges = parse_result_graph(output['data'], name_col)
     output = {'nodes':nodes, 'edges':edges}
 
     return output
